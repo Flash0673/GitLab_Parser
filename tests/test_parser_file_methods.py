97f1dfa71ee1481e7db5a81cac6a857a06433c42
Flash0673 <111455730+Flash0673@users.noreply.github.com>
2023-06-01 01:52:59 +0300

    Update test_parser_file_methods.py

--- tests/test_parser_file_methods.py
+++ tests/test_parser_file_methods.py
@@ -2,7 +2,7 @@ from Parser import Parser
 
 
 def test_get_merge_requests_file():
-    p = Parser(token="BxntftQ1zwq_28vtS2Qm")
+    p = Parser(token="YOUR_TOKEN")
     p.get_merge_requests_file(input_file="Data/test.json",
                               output_file="Data/get_merge_requests_file/test_output_get_merge_requests_file.json")
     with open("Data/get_merge_requests_file/output_get_merge_requests_file.json") as f:
@@ -13,7 +13,7 @@ def test_get_merge_requests_file():
 
 
 def test_get_merge_commits_file():
-    p = Parser(token="BxntftQ1zwq_28vtS2Qm")
+    p = Parser(token="YOUR_TOKEN")
     p.get_merge_commits_file(input_file="Data/get_merge_requests_file/output_get_merge_requests_file.json",
                              output_file="Data/get_merge_commits_file/test_output_get_merge_commits_file.csv")
     with open("Data/get_merge_commits_file/output_get_merge_commits_file.csv") as f:
